36ffd0380cebbbf82cac522767a0032908817c6b
cjr-- <cjr@hotmail.co.uk>
2021-02-26 00:45:33 +0000

    .

--- .bash_aliases
+++ .bash_aliases
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alias ls='ls -F --color=auto'
+alias ls='ls -lhg --color=auto --group-directories-first'
 alias ll='ls -vlAhg --group-directories-first'
 
 alias cls='clear'
--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-"${HOME}"/.dotfiles/scripts/install_common.sh
+if [[ "$*" =~ "--install" ]]; then
+  "${HOME}"/.dotfiles/scripts/install_common.sh  
+fi
+
 "${HOME}"/.dotfiles/scripts/link_files.sh
 "${HOME}"/.dotfiles/scripts/copy_files.sh
