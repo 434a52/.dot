06ec192cf13d704e3c8a8aa88f93336752450ee1
cjr-- <cjr@hotmail.co.uk>
2021-03-06 09:00:18 +0000

    .

--- install/ubuntu_tools.sh
+++ install/ubuntu_tools.sh
@@ -15,6 +15,11 @@ apt_install tmux
 apt_install micro
 
 if [ "${OS_TYPE}" = "DESKTOP" ]; then
+
+  gsettings set org.gnome.desktop.background picture-options 'none'
+  gsettings set org.gnome.desktop.background primary-color '#000000'
+
   apt_install gnome-tweaks
   apt_install fonts-firacode
+
 fi
