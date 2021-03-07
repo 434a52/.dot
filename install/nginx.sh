ff665f88ce5ff19c0fa74f1ec52768a33bceefa5
Chris Richardson <cjr@hotmail.co.uk>
2021-03-07 14:04:29 +0000

    .

--- install/nginx.sh
+++ install/nginx.sh
@@ -3,12 +3,6 @@ set -e
 
 if [ -z "$(which nginx)" ]; then
 
-  sudo apt install -y curl gnupg2 ca-certificates lsb-release
-  echo "deb http://nginx.org/packages/ubuntu $(lsb_release -cs) nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
-
-  curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
-
-  sudo apt update
   sudo apt install -y nginx
 
 fi
